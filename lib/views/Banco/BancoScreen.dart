75e2b5a8e784044a9b1849ced5a57397652029d5
Miqueias Cardoso <miqueiascardoso@MacBook-Air-de-Miqueias.local>
2023-09-27 19:53:26 -0400

    tela bancos com api e botao add para criar formulario de autenticacao

--- lib/views/Banco/BancoScreen.dart
+++ lib/views/Banco/BancoScreen.dart
@@ -10,39 +10,67 @@ class BancoScreen extends StatefulWidget {
   State<BancoScreen> createState() => _BancoScreenState();
 }
 
-List<Banco> listaBancos = [];
-
-void getbancosFromApi() async {
-  listaBancos = await getBanco(ip: AppRoutes.Ipservidor);
-}
-
 class _BancoScreenState extends State<BancoScreen> {
+  List<Banco> listaBancos = [];
+  bool _isLoading = false;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _isLoading = true;
     getbancosFromApi();
   }
 
+  void getbancosFromApi() async {
+    var value = await getBanco(ip: AppRoutes.Ipservidor);
+    setState(() {
+      listaBancos = value;
+    });
+    _isLoading = false;
+  }
+
+  var appBar = AppBar(
+    backgroundColor: Color.fromARGB(255, 206, 196, 227),
+    title: Text(
+      'Lista de Bancos',
+      style: TextStyle(color: Colors.white),
+    ),
+    centerTitle: true,
+  );
+
   @override
   Widget build(BuildContext context) {
+    var maxSizeUtilBody = (MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top);
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 60, 39, 176),
-        title: Text(
-          'Lista de Bancos',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-      ),
-      body: Container(
-        color: const Color.fromARGB(255, 255, 255, 255),
-        child: ListView.builder(
-          itemCount: listaBancos!.length,
-          itemBuilder: ((context, index) => ListTile(
-                title: Text('${listaBancos![index].banco}'),
-              )),
-        ),
+      appBar: appBar,
+      body: Column(
+        children: [
+          Container(
+            child: _isLoading
+                ? LinearProgressIndicator(
+                    minHeight: 5,
+                  )
+                : Container(
+                    height: maxSizeUtilBody * 0.85,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView.builder(
+                      itemCount: listaBancos.length,
+                      itemBuilder: ((context, index) => ListTile(
+                            title: Text('${listaBancos![index].banco}'),
+                          )),
+                    ),
+                  ),
+          ),
+          _isLoading
+              ? Container()
+              : FloatingActionButton(
+                  child: Icon(Icons.add),
+                  onPressed: () {},
+                ),
+        ],
       ),
     );
   }
